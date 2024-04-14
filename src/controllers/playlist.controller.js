3bd161b1ccabb709c64fedea7ec0ec4d0c30bca2
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-04-15 01:00:18 +0530

    "Add playlist creation with validation and authentication; update controller and route"
    This commit adds playlist creation functionality with validation and authentication. The `createPlaylist` controller function now checks for empty fields and creates a playlist with the provided

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,11 +1,48 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
+import Playlist from "../models/playlist.model.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
     //TODO: create playlist
-    const {name, description} = req.body
+    /*
+    1. take playlistName, description from req.body
+    2. check any field is empty or not
+    3. if empty, throw an error
+    4. create playlist
+    5. return a response
+    */
+
+    const {playlistName, description} = req.body
+    if ([playlistName, description].some((field) => field?.trim() === "")) {
+        throw new ApiError(404, "Playlist name & description is needed")
+    }
+    if (!playlistName || !description) {
+        throw new ApiError(404, "Playlist name & description should be given")
+    }
     
+    const playlist = await Playlist.create(
+        {
+            playlistName: playlistName.trim(),
+            description: description.trim(),
+            owner: req.user._id
+        }
+    )
+
+    const createdPlaylist = await Playlist.findById(playlist._id)
+    if (!createdPlaylist) {
+        throw new ApiError(500, "Something went wrong while creating the playlist")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(
+            200,
+            createdPlaylist,
+            "Playlist is successfully created"
+        )
+    )
 })
 
 export {
--- src/routes/playlisy.route.js
+++ src/routes/playlisy.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import verifyJWT from "../middlewares/auth.middleware.js";
 import { 
     createPlaylist 
 } from "../controllers/playlist.controller.js";
 
 const router = Router()
 
-router.route("/create").get(createPlaylist)
+router.route("/create").post(verifyJWT, createPlaylist)
 
 export default router
