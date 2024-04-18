3d4f8955c80bbaba12e4991a9b44b58301712a7b
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-04-18 23:18:57 +0530

    Modify tweet.controller.js controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -2,7 +2,6 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import Tweet from "../models/tweets.model.js";
-import { isValidObjectId } from "mongoose";
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
