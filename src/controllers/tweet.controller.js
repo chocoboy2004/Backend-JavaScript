88150a8c68653a91558b71396b71c103e8c171e4
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-04-18 17:29:33 +0530

    "Add validation and response handling to createTweet controller"
    The diff includes the following changes to the `createTweet` controller function:
    
    * Adding validation for the existence of a user from the request object.
    * Add

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -5,10 +5,36 @@ import Tweet from "../models/tweets.model.js";
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
-    res.send(
+    const user = req.user._id
+    if (!user) {
+        throw new ApiError(404, "Please login or signup")
+    } 
+
+    const { content } = req.body
+    if (!content || content.length < 1) {
+        throw new ApiError(404, "Content is required")
+    }
+
+    const tweet = await Tweet.create(
         {
-            message: "OK"
+            owner: user,
+            content: content.trim()
         }
+    )
+
+    const createdTweet = await Tweet.findById(tweet._id)
+    if (!createdTweet) {
+        throw new ApiError(500, "Something went wrong while creating tweet")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(
+            200,
+            createdTweet,
+            "Tweet is posted successfully"
+        )
     )
 })
 
