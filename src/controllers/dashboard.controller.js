9aee7abe3752b182be2f63da4c84e40bc172981b
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-04-20 13:44:35 +0530

    "Add controller function to fetch videos by channel; use Mongoose for database operations"
    This commit includes the following changes:
    
    * Importing Mongoose and Video model from their respective modules
    * Adding a console log statement to print

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,14 +1,67 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
+import { Video } from "../models/video.models.js";
+
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
-    res.send(
-        {
-            message: "OK"
+    const channel = req.user._id
+    console.log(channel)
+    if (!channel) {
+        throw new ApiError(404, "Please login first")
+    }
+
+    try {
+        const videos = await Video.aggregate(
+            [
+                {
+                    $match: {
+                        owner: new mongoose.Types.ObjectId(channel)
+                    }
+                },
+                {
+                    $project: {
+                        videoFile: 1,
+                        thumbnail: 1,
+                        title: 1,
+                        description: 1,
+                        owner: 1,
+                        views: 1,
+                        createdAt: 1
+                    }
+                }
+            ]
+        )
+
+        if (videos.length > 0) {
+            return res
+            .status(201)
+            .json(
+                new ApiResponse(
+                    200,
+                    videos,
+                    "Videos are fetched successfully"
+                )
+            )
+        } else {
+            return res
+            .status(201)
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        message: "There is no video available"
+                    },
+                    "Video are fetched successfully"
+                )
+            )
         }
-    )
+    } catch (error) {
+        console.error(`Error occured in try block: ${error.message}`)
+        process.exit(1)
+    }
 })
 
 export {
