85b6819fdf4f3e0e4db54814896f2c02356e9d90
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-04-20 16:32:33 +0530

    "Add getChannelStats endpoint to dashboard controller and route"
    This diff adds a new endpoint, getChannelStats, to the dashboard controller and route. The endpoint fetches channel stats such as total video views, total subscribers, total likes and total views

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -3,6 +3,8 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
 import { Video } from "../models/video.models.js";
+import { Subscription } from "../models/subscription.model.js";
+import Like from "../models/likes.model.js";
 
 
 const getChannelVideos = asyncHandler(async (req, res) => {
@@ -37,26 +39,26 @@ const getChannelVideos = asyncHandler(async (req, res) => {
 
         if (videos.length > 0) {
             return res
-            .status(201)
-            .json(
-                new ApiResponse(
-                    200,
-                    videos,
-                    "Videos are fetched successfully"
+                .status(201)
+                .json(
+                    new ApiResponse(
+                        200,
+                        videos,
+                        "Videos are fetched successfully"
+                    )
                 )
-            )
         } else {
             return res
-            .status(201)
-            .json(
-                new ApiResponse(
-                    200,
-                    {
-                        message: "There is no video available"
-                    },
-                    "Video are fetched successfully"
+                .status(201)
+                .json(
+                    new ApiResponse(
+                        200,
+                        {
+                            message: "There is no video available"
+                        },
+                        "Video are fetched successfully"
+                    )
                 )
-            )
         }
     } catch (error) {
         console.error(`Error occured in try block: ${error.message}`)
@@ -64,6 +66,154 @@ const getChannelVideos = asyncHandler(async (req, res) => {
     }
 })
 
+const getChannelStats = asyncHandler(async (req, res) => {
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const channel = req.user._id
+    if (!channel) {
+        throw new ApiError(404, "Please login first")
+    }
+
+    try {
+        const subscribers = await Subscription.aggregate(
+            [
+                {
+                    $match: {
+                        channel: new mongoose.Types.ObjectId(channel)
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "subscriber",
+                        foreignField: "_id",
+                        as: "subscribers"
+                    }
+                },
+                {
+                    $addFields: {
+                        subscribers: {
+                            $size: "$subscribers"
+                        }
+                    }
+                },
+                {
+                    $group: {
+                        _id: null,
+                        subscribers: {
+                            $sum: 1
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        subscribers: 1
+                    }
+                }
+            ]
+        )
+    
+        const videos = await Video.aggregate(
+            [
+                {
+                    $match: {
+                        owner: new mongoose.Types.ObjectId(channel)
+                    }
+                },
+                {
+                    $project: {
+                        videoFile: 1,
+                        thumbnail: 1,
+                        title: 1,
+                        description: 1,
+                        views: 1,
+                        createdAt: 1,
+                        owner: 1
+                    }
+                }
+            ]
+        )
+    
+        const totalLikes = await Like.aggregate(
+            [
+                {
+                    $match: {
+                        likedBy: new mongoose.Types.ObjectId(channel),
+                        video: {
+                            $exists: true
+                        }
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "videos",
+                        localField: "likedBy",
+                        foreignField: "owner",
+                        as: "result"
+                    }
+                },
+                {
+                    $group: {
+                        _id: null,
+                        likes: {
+                            $sum: 1
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        likes: 1
+                    }
+                }
+            ]
+        )
+    
+        const totalViews = await Video.aggregate(
+            [
+                {
+                    $match: {
+                        owner: new mongoose.Types.ObjectId(channel)
+                    }
+                },
+                {
+                    $group: {
+                        _id: null,
+                        views: {
+                            $sum: "$views"
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        views: 1
+                    }
+                }
+            ]
+        )
+    
+        return res
+            .status(201)
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        subscribers: subscribers,
+                        likes: totalLikes,
+                        views: totalViews,
+                        videos: videos
+                    },
+                    "Channel stats are fecthed"
+                )
+            )
+    } catch (error) {
+        console.error(`Error occured in try block: ${error.message}`)
+        process.exit(1)
+    }
+})
+
 export {
-    getChannelVideos
+    getChannelVideos,
+    getChannelStats
 }
--- src/routes/dashboard.route.js
+++ src/routes/dashboard.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.middleware.js";
 import { 
+    getChannelStats,
     getChannelVideos 
 } from "../controllers/dashboard.controller.js";
 
@@ -8,5 +9,6 @@ const router = Router()
 router.use(verifyJWT)
 
 router.route("/videos").get(getChannelVideos)
+router.route("/stats").get(getChannelStats)
 
 export default router
